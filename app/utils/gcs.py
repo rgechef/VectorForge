46aa405b2a81b22cbb2ac1791ea15094b2d97040
Ronald Estes <64745255+rgechef@users.noreply.github.com>
2025-07-29 14:49:39 -0400

    Update gcs.py

--- app/utils/gcs.py
+++ app/utils/gcs.py
@@ -5,5 +5,5 @@ def upload_file_to_gcs(file_data, filename, bucket_name="3dshapesnap-uploads"):
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(filename)
     blob.upload_from_string(file_data)
-    blob.make_public()
+    blob.make_public()  # Comment out if you want to keep private
     return blob.public_url
