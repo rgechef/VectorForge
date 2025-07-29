8291f55f32b4691331c99b086ca3c4966aa6e7f8
Ronald Estes <64745255+rgechef@users.noreply.github.com>
2025-07-29 14:47:04 -0400

    Update gcs.py

--- app/utils/gcs.py
+++ app/utils/gcs.py
@@ -1,10 +1,9 @@
 from google.cloud import storage
 
-def upload_file_to_gcs(file_data, filename, bucket_name):
+def upload_file_to_gcs(file_data, filename, bucket_name="3dshapesnap-uploads"):
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(filename)
     blob.upload_from_string(file_data)
-    # If you want public access:
     blob.make_public()
     return blob.public_url
