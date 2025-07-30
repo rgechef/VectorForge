e0624ad96fe3731d2a45708b04a689fc8664c834
Ronald Estes <64745255+rgechef@users.noreply.github.com>
2025-07-30 05:55:38 -0400

    Update gcs.py

--- app/utils/gcs.py
+++ app/utils/gcs.py
@@ -1,9 +1,23 @@
+# app/utils/gcs.py
+import os
 from google.cloud import storage
 
-def upload_file_to_gcs(file_data, filename, bucket_name="3dshapesnap-uploads"):
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(filename)
-    blob.upload_from_string(file_data)
-    blob.make_public()  # Comment out if you want to keep private
+GCS_BUCKET = os.getenv("GCS_BUCKET", "vectorforge-uploads")
+GCS_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "/etc/secrets/gcp-key.json")
+
+def get_gcs_client():
+    return storage.Client.from_service_account_json(GCS_CREDENTIALS)
+
+def upload_to_gcs(file_obj, filename, folder="outputs"):
+    client = get_gcs_client()
+    bucket = client.bucket(GCS_BUCKET)
+    blob = bucket.blob(f"{folder}/{filename}")
+    blob.upload_from_file(file_obj)
+    blob.make_public()
     return blob.public_url
+
+def download_from_gcs(filename, folder="outputs"):
+    client = get_gcs_client()
+    bucket = client.bucket(GCS_BUCKET)
+    blob = bucket.blob(f"{folder}/{filename}")
+    return blob.download_as_bytes()
